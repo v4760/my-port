7b5a714aca744cf834ccfe02b4cc545e33de4419
Vikas Kagawad <vikaskagawad@gmail.com>
2024-02-15 09:19:54 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = "";
+const base = "/my-port";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
